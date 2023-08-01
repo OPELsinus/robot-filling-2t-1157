7632ed639f6c3f9c1d83255d4666d5bcbdd2d4de
Abdykarim.D <Abdykarim.D@magnum.kz>
2023-08-01 08:45:16 +0600

    ghfg

--- main.py
+++ main.py
@@ -296,7 +296,7 @@ def start_single_branch(filepath, store, values_first_part, values_second_part):
     sleep(1)
     sign_ecp(ecp_auth)
 
-    logged_in = web.wait_element('//*[@id="idLogout"]/a')
+    logged_in = web.wait_element('//*[@id="idLogout"]/a', timeout=35)
 
     if logged_in:
         if web.find_element("//a[text() = 'Выйти']"):
@@ -342,8 +342,8 @@ def start_single_branch(filepath, store, values_first_part, values_second_part):
             # wait_loading(web, '//*[@id="radio-1132-boxLabelEl"]')  # ? УБРАТЬ В БОЮ
             # wait_loading(web, '//*[@id="radio-1131-boxLabelEl"]')  # ? УБРАТЬ В БОЮ
             # wait_loading(web, '//*[@id="radio-1132-boxLabelEl"]')  # ? УБРАТЬ В БОЮ
-            # web.find_element('//*[@id="radio-1132-boxLabelEl"]').click()
-
+            # # web.find_element('//*[@id="radio-1132-boxLabelEl"]').click()
+            # sleep(10)
             if web.wait_element("//div[contains(text(), '2-торговля')]", timeout=5):
                 web.find_element("//div[contains(text(), '2-торговля')]").click()
             else:
@@ -398,9 +398,9 @@ def start_single_branch(filepath, store, values_first_part, values_second_part):
             web.find_element("//a[contains(text(), 'Страница 1')]").click()
 
             web.find_element('//*[@id="rtime"]').select('2')
-
+            sleep(1)
             for ind, group in enumerate(groups):
-
+                # sleep(1)
                 if group == 'Объем розничной торговли':
                     try:
                         web.execute_script(xpath=f"//*[contains(text(), '{group}')]/following-sibling::*[contains(@role, 'gridcell')][1]", value=str(values_first_part[0]))
@@ -420,9 +420,10 @@ def start_single_branch(filepath, store, values_first_part, values_second_part):
 
                 else:
                     try:
-                        web.execute_script(xpath=f"//*[contains(text(), '{group}')]/following-sibling::*[contains(@role, 'gridcell')][2]", value=str(round(values_second_part.get(group))))
+                        web.execute_script(xpath=f"//*[contains(text(), '{group}')]/following-sibling::*[contains(@role, 'gridcell')][2]", value=str(round(values_second_part.get(group)) / 1000))
                     except:
                         web.execute_script(xpath=f"//*[contains(text(), '{group}')]/following-sibling::*[contains(@role, 'gridcell')][2]", value='0')
+            sleep(0.1)
             logger.info('Check4')
             web.find_element("//a[contains(text(), 'Данные исполнителя')]").click()
             web.execute_script(element_type="value", xpath="//*[@id='inpelem_1_0']", value='Қалдыбек Б.Ғ.')
@@ -430,25 +431,57 @@ def start_single_branch(filepath, store, values_first_part, values_second_part):
             web.execute_script(element_type="value", xpath="//*[@id='inpelem_1_2']", value='87073332438')
             web.execute_script(element_type="value", xpath="//*[@id='inpelem_1_3']", value='KALDYBEK.B@magnum.kz')
 
-            # web.execute_script_click_xpath("//span[text() = 'Сохранить']")
+            web.execute_script_click_xpath("//span[text() = 'Сохранить']")
+            sleep(1)
+            print('Clicked Save')
+            if web.wait_element("//span[text() = 'Сохранить отчет и Удалить другие']", timeout=5):
+                web.execute_script_click_xpath("//span[text() = 'Сохранить отчет и Удалить другие']")
+            print('Clicking Send')
+            web.execute_script_click_xpath("//span[text() = 'Отправить']")
+            print('Clicked Send')
+            web.wait_element("//input[@value = 'Персональный компьютер']", timeout=30)
+            web.execute_script_click_xpath("//input[@value = 'Персональный компьютер']")
+
+            sign_ecp(ecp_sign)
+            # sleep(1000)
             # print(store.replace('Торговый зал', '').replace(' ', '').replace('№', ''))
             # sleep(30)
+
+            found = False
+            while True:
+                for file in os.listdir(download_path):
+                    if '.jpg' in file and 'crdownload' not in file:
+                        shutil.copy(os.path.join(download_path, file), os.path.join(os.path.join(download_path, 'Отчёты 2т'), branch))
+                        print(file)
+                        found = True
+                        break
+                if found:
+                    break
+
             web.close()
             web.quit()
 
-            print('Return this shit')
+            print('Successed')
             return ['success', '']
+
+    else:
+        web.close()
+        web.quit()
+
+        print('Srok istek')
+        return ['failed', 'Срок ЭЦП истёк']
 
 
 def get_data_from_1157(branch):
 
     for file in os.listdir(os.path.join(download_path, 'Splitted')):
-        if branch in file:
+        if branch + '_' in file:
             print('Read', os.path.join(os.path.join(download_path, 'Splitted'), file))
             return pd.read_excel(os.path.join(os.path.join(download_path, 'Splitted'), file), header=0)
 
 
 if __name__ == '__main__':
+
     sql_create_table()
     # start = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S.%f")
     # insert_data_in_db(started_time=start, store_id=2350, store_name='Loh', status='failed', error_reason='guano', error_saved_path='', execution_time='10s')
@@ -462,22 +495,31 @@ if __name__ == '__main__':
 
     df = pd.DataFrame(columns=['id', 'branch', 'data'])
 
-    for file in os.listdir(os.path.join(download_path, 'downloads1')):
+    for file in os.listdir(download_path):
 
         if file == '!result.xlsx':
 
-            book = load_workbook(os.path.join(os.path.join(download_path, 'downloads1'), file))
+            book = load_workbook(os.path.join(download_path, file))
             sheet = book.active
+            month = datetime.datetime.today().month
 
+            months = 'DEFGHIJKLMNO'
+            print('MON:', month, months[month - 1])
             while sheet[f'A{counter}'].value is not None:
-                all_branches.append([sheet[f'A{counter}'].value, sheet[f'B{counter}'].value, [sheet[f'I{counter}'].value, sheet[f'I{counter + 1}'].value, sheet[f'I{counter + 2}'].value]])
+                all_branches.append([sheet[f'A{counter}'].value, sheet[f'B{counter}'].value, [sheet[f'{months[month - 1]}{counter}'].value, sheet[f'{months[month - 1]}{counter + 1}'].value, sheet[f'{months[month - 1]}{counter + 2}'].value]])
 
-                row = pd.DataFrame({'id': sheet[f'A{counter}'].value, 'branch': sheet[f'B{counter}'].value, 'data': [[sheet[f'I{counter}'].value, sheet[f'I{counter + 1}'].value, sheet[f'I{counter + 2}'].value]]})
+                row = pd.DataFrame({'id': sheet[f'A{counter}'].value, 'branch': sheet[f'B{counter}'].value, 'data': [[sheet[f'{months[month - 1]}{counter}'].value, sheet[f'{months[month - 1]}{counter + 1}'].value, sheet[f'{months[month - 1]}{counter + 2}'].value]]})
 
                 df = pd.concat([df, row], ignore_index=True)
                 counter += 3
 
-    df1 = get_all_branches_with_codes()
+    while True:
+        try:
+            df1 = get_all_branches_with_codes()
+            break
+        except:
+            print('Error with adb')
+            sleep(120)
 
     df['name'] = None
     df['store_id'] = None
@@ -492,26 +534,32 @@ if __name__ == '__main__':
 
     status = 'succ1ess'
 
-    if status != 'success':
-        all_rows = get_all_data()
-
-        all_bad_rows = all_rows[all_rows['status'] != 'success']
-
-        all_bad_rows['store_normal_name'] = None
-        all_bad_rows = all_bad_rows.reset_index(inplace=False)
-        for i in range(len(all_bad_rows)):
-            all_bad_rows.loc[i, 'store_normal_name'] = df1[df1['store_id'] == all_bad_rows['store_id'].iloc[i]]['store_normal_name'].iloc[0]
-
-        df_prev = df.copy()
-        df = all_bad_rows
-        df['data'] = df_prev['data']
+    # if status != 'success':
+    #     all_rows = get_all_data()
+    #
+    #     all_bad_rows = all_rows[all_rows['status'] != 'success']
+    #
+    #     all_bad_rows['store_normal_name'] = None
+    #     all_bad_rows = all_bad_rows.reset_index(inplace=False)
+    #     for i in range(len(all_bad_rows)):
+    #         all_bad_rows.loc[i, 'store_normal_name'] = df1[df1['store_id'] == all_bad_rows['store_id'].iloc[i]]['store_normal_name'].iloc[0]
+    #
+    #     df_prev = df.copy()
+    #     df = all_bad_rows
+    #     df['data'] = df_prev['data']
 
     print('Len:', len(df))
-
+    check = False
     for ind, branch in enumerate(df['name']):
 
-        # if branch != 'Торговый зал АСФ №80':
+        # if branch == 'Торговый зал АСФ №6':
         #     continue
+
+        if branch == 'Торговый зал АСФ №69':
+            check = True
+        if not check:
+            print(branch)
+            continue
 
         ecp_path = fr'\\vault.magnum.local\common\Stuff\_06_Бухгалтерия\! Актуальные ЭЦП\{branch}'
 
@@ -544,6 +592,7 @@ if __name__ == '__main__':
                     insert_data_in_db(started_time=start, store_id=int(df['store_id'].iloc[ind]), store_name=str(branch), status='failed', error_reason=status, error_saved_path=saved_path, execution_time=str(end_time - start_time))
                 else:
                     insert_data_in_db(started_time=start, store_id=int(df['store_id'].iloc[ind]), store_name=str(branch), status='success', error_reason='', error_saved_path='', execution_time=str(end_time - start_time))
+
                 send_message_to_tg(tg_token, chat_id, f'Finished, {ind}, {branch}')
 
             except Exception as ebanko:
